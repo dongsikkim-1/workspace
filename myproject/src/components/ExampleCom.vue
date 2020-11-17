0ae6385a5b2b4045cd8a6d5ddbb1cc0fc9c1cca1
wininfo <wininfo@wininfoui-MacBookPro.local>
2020-11-17 15:29:57 +0900

    first commit

---
+++ myproject/src/components/ExampleCom.vue
@@ -0,0 +1,44 @@
+<template>
+<v-app>
+  <div class="hello">
+    <h1>{{ msg }}</h1><h1>{{title}}</h1>
+    <h2>Example Compinent 01</h2>
+  </div>
+  <v-row>
+      <v-col>Test
+      </v-col>
+  </v-row>
+</v-app>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: ''
+    }
+  },
+  props : {
+      title : ''
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
