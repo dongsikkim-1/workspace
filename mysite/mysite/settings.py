757749215ad4c81ff6676d550e65ba3c1614daa4
wininfo <wininfo@wininfoui-MacBookPro.local>
2020-11-23 11:03:00 +0900

    .

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -76,9 +76,16 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+    'default':{
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'test',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '1521'
     }
 }
 
