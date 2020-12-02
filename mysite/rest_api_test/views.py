a9e8344afc707baab318c16e0b10fdfc9c157237
wininfo <wininfo@wininfoui-MacBookPro.local>
2020-12-02 16:45:40 +0900

    ...

--- mysite/rest_api_test/views.py
+++ mysite/rest_api_test/views.py
@@ -2,22 +2,27 @@
 
 # Create your views here.
 
-
 from django.views import View
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
+from django.db.models import F
+from models import employees
 
 @method_decorator(csrf_exempt,name='dispatch')
 class IndexView(View):
     def get(self, request):
-        dummy_data = {
-            'name': '죠르디',
-            'type': '공룡',
-            'job': '편의점알바생',
-            'age': 5
-        }
-        return JsonResponse(dummy_data)
+
+        emps = employees.objects.filter(scoops_ordered__gt=F('store_visits'))
+        
+        # dummy_data = {
+        #     'name': '죠르디',
+        #     'type': '공룡',
+        #     'job': '편의점알바생',
+        #     'age': 5
+        # }
+        # return JsonResponse(dummy_data)
+        return JsonResponse(emps)
     
     def post(self, request):
         return HttpResponse("Post 요청을 잘받았다")
