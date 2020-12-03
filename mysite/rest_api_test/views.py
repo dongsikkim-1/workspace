1aaa0d6330de224d32eb70d8fcda5684a0f5201e
wininfo <wininfo@wininfoui-MacBookPro.local>
2020-12-04 08:01:41 +0900

    ..

--- mysite/rest_api_test/views.py
+++ mysite/rest_api_test/views.py
@@ -51,9 +51,11 @@ class IndexView(View):
         if request.method == 'POST': 
             note_data = request.POST 
             login_user = request.user
-            data = employees(draft_user=login_user,note_data=note_data.get('note_data'),created_at=datetime.datetime.now(),updated_at=datetime.datetime.now()) 
+            data = employees(draft_user=login_user
+            ,note_data=note_data.get('note_data')
+            ,created_at=datetime.datetime.now(),updated_at=datetime.datetime.now()) 
             data.save() 
-        return return HttpResponse("Post 요청을 잘받았다")
+        return HttpResponse("Post 요청을 잘받았다")
 
         # return HttpResponse("Post 요청을 잘받았다")
    
